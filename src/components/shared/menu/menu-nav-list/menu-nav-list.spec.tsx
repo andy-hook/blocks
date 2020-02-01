ce02aa4259fe2d55e698e7596633d39cb5f5aa2d
Andy Hook <hello@andy-hook.co.uk>
2020-02-01 18:14:23 +0000

    Fix test issue with deps

--- src/components/shared/menu/menu-nav-list/menu-nav-list.spec.tsx
+++ src/components/shared/menu/menu-nav-list/menu-nav-list.spec.tsx
@@ -4,7 +4,7 @@ import MenuNavList from "./menu-nav-list"
 import { renderWithTheme } from "@test-utils"
 
 describe("<MenuNavList />", () => {
-  it("renders correctly", () => {
+  fit("renders correctly", () => {
     const tree = renderWithTheme(
       "light",
       <MenuNavList onClick={jest.fn} open={true} />
