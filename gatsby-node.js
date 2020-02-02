b20a53b0be99d587ccb666d4bbe912feeefd3d11
Andy Hook <hello@andy-hook.co.uk>
2020-02-02 10:23:27 +0000

    Redirect away from /block template

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,3 +3,25 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+
+exports.createPages = ({ actions }) => {
+  // need createRedirect action in actions collection
+  // to make the redirection magic happen.
+  // https://www.gatsbyjs.org/docs/bound-action-creators/
+  const { createRedirect } = actions
+
+  // Redirect blocks to index
+  createRedirect({
+    fromPath: `/blocks`,
+    toPath: `/`,
+    isPermanent: true,
+    redirectInBrowser: true,
+  })
+
+  createRedirect({
+    fromPath: `/blocks/`,
+    toPath: `/`,
+    isPermanent: true,
+    redirectInBrowser: true,
+  })
+}
