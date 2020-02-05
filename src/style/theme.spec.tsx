a4624588274c912d55aa75118352df0376944d98
Andy Hook <hello@andy-hook.co.uk>
2020-02-05 23:21:23 +0000

    Move colour tokens to theme file

--- src/style/theme.spec.tsx
+++ src/style/theme.spec.tsx
@@ -8,6 +8,14 @@ import {
   isDarkTheme,
   isLightTheme,
   isTheme,
+  darkThemeText,
+  darkThemeTextAlpha,
+  darkThemeTone,
+  darkThemeToneAlpha,
+  lightThemeText,
+  lightThemeTextAlpha,
+  lightThemeTone,
+  lightThemeToneAlpha,
 } from "@style/theme"
 import styled from "styled-components"
 import "jest-styled-components"
@@ -43,6 +51,54 @@ const IsThemeComponent = styled.div`
 const IsNotThemeComponent = styled.div`
   ${isTheme("dark", "color: red;", "color: blue;")};
 `
+
+describe("darkThemeText", () => {
+  test("should return correct value as hsl string", () => {
+    expect(darkThemeText(100)).toEqual("hsl(240, 3%, 100%)")
+  })
+})
+
+describe("darkThemeTextAlpha", () => {
+  test("should return correct value as hsla string", () => {
+    expect(darkThemeTextAlpha(100, 50)).toEqual("hsla(240, 3%, 100%,50)")
+  })
+})
+
+describe("darkThemeTone", () => {
+  test("should return correct value as hsl string", () => {
+    expect(darkThemeTone(100)).toEqual("hsl(240, 6%, 7%)")
+  })
+})
+
+describe("darkThemeToneAlpha", () => {
+  test("should return correct value as hsla string", () => {
+    expect(darkThemeToneAlpha(100, 50)).toEqual("hsla(240, 6%, 7%,50)")
+  })
+})
+
+describe("lightThemeText", () => {
+  test("should return correct value as hsl string", () => {
+    expect(lightThemeText(100)).toEqual("hsl(240, 3%, 100%)")
+  })
+})
+
+describe("lightThemeTextAlpha", () => {
+  test("should return correct value as hsla string", () => {
+    expect(lightThemeTextAlpha(100, 50)).toEqual("hsla(240, 3%, 100%,50)")
+  })
+})
+
+describe("lightThemeTone", () => {
+  test("should return correct value as hsl string", () => {
+    expect(lightThemeTone(100)).toEqual("hsl(240, 3%, 100%)")
+  })
+})
+
+describe("lightThemeToneAlpha", () => {
+  test("should return correct value as hsla string", () => {
+    expect(lightThemeToneAlpha(100, 50)).toEqual("hsla(240, 3%, 100%,50)")
+  })
+})
 
 describe("themeTone", () => {
   it("renders correct hsl from given tone value", () => {
--- src/style/theme.tsx
+++ src/style/theme.tsx
@@ -1,21 +1,100 @@
-import { Theme, GreyNames, ThemeName } from "@custom-types/theme"
+import { Theme, GreyNames, ThemeName, Greys } from "@custom-types/theme"
 import { css, CSSProp } from "styled-components"
 import { createHsl, createHsla } from "@style/utils"
 
-import {
-  darkThemeTextHSL,
-  darkThemeToneHSL,
-  lightThemeTextHSL,
-  lightThemeToneHSL,
-} from "@style/variables"
+/* Dark theme greys
+------------------------------------------------- */
 
-export const lightTheme: Theme = {
+// Tone
+const darkThemeToneHSL: Greys = {
+  100: "240, 6%, 7%",
+  200: "240, 5%, 8%",
+  300: "240, 4%, 10%",
+  400: "240, 4%, 11%",
+  500: "240, 2%, 12%",
+  600: "240, 3%, 13%",
+  700: "240, 3%, 14%",
+  800: "240, 3%, 15%",
+  900: "240, 2%, 16%",
+  1000: "240, 2%, 17%",
+}
+
+export const darkThemeTone = (value: GreyNames) =>
+  createHsl(darkThemeToneHSL[value])
+
+export const darkThemeToneAlpha = (value: GreyNames, alpha: number) =>
+  createHsla(darkThemeToneHSL[value], alpha)
+
+// Text
+const darkThemeTextHSL: Greys = {
+  100: "240, 3%, 100%",
+  200: "240, 3%, 97%",
+  300: "240, 3%, 95%",
+  400: "240, 3%, 93%",
+  500: "240, 3%, 90%",
+  600: "240, 3%, 86%",
+  700: "240, 3%, 83%",
+  800: "240, 3%, 60%",
+  900: "240, 3%, 40%",
+  1000: "240, 3%, 30%",
+}
+
+export const darkThemeText = (value: GreyNames) =>
+  createHsl(darkThemeTextHSL[value])
+
+export const darkThemeTextAlpha = (value: GreyNames, alpha: number) =>
+  createHsla(darkThemeTextHSL[value], alpha)
+
+/* Light theme greys
+------------------------------------------------- */
+
+// Tone
+const lightThemeToneHSL: Greys = {
+  100: "240, 3%, 100%",
+  200: "240, 3%, 97%",
+  300: "240, 3%, 95%",
+  400: "240, 3%, 93%",
+  500: "240, 3%, 90%",
+  600: "240, 3%, 86%",
+  700: "240, 3%, 83%",
+  800: "240, 3%, 60%",
+  900: "240, 3%, 40%",
+  1000: "240, 3%, 30%",
+}
+
+export const lightThemeTone = (value: GreyNames) =>
+  createHsl(lightThemeToneHSL[value])
+
+export const lightThemeToneAlpha = (value: GreyNames, alpha: number) =>
+  createHsla(lightThemeToneHSL[value], alpha)
+
+// Text
+const lightThemeTextHSL: Greys = {
+  100: "240, 17%, 2%",
+  200: "240, 15%, 5%",
+  300: "240, 15%, 11%",
+  400: "240, 10%, 14%",
+  500: "240, 8%, 18%",
+  600: "240, 8%, 21%",
+  700: "240, 8%, 26%",
+  800: "240, 8%, 30%",
+  900: "240, 8%, 40%",
+  1000: "240, 8%, 60%",
+}
+
+export const lightThemeText = (value: GreyNames) =>
+  createHsl(darkThemeTextHSL[value])
+
+export const lightThemeTextAlpha = (value: GreyNames, alpha: number) =>
+  createHsla(darkThemeTextHSL[value], alpha)
+
+const lightTheme: Theme = {
   name: "light",
   text: lightThemeTextHSL,
   tone: lightThemeToneHSL,
 }
 
-export const darkTheme: Theme = {
+const darkTheme: Theme = {
   name: "dark",
   text: darkThemeTextHSL,
   tone: darkThemeToneHSL,
