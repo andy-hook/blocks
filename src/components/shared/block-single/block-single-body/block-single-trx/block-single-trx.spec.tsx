665588d692b46bb7b6b30c8e6cd1f9197a5e46aa
Andy Hook <hello@andy-hook.co.uk>
2020-02-14 19:23:48 +0000

    Organize block-single component structure

--- src/components/shared/block-single/block-single-body/block-single-trx/block-single-trx.spec.tsx
+++ src/components/shared/block-single/block-single-body/block-single-trx/block-single-trx.spec.tsx
@@ -1,12 +1,16 @@
 import React from "react"
 import BlockSingleTrx from "./block-single-trx"
 import { renderWithTheme } from "@test-utils"
+import { mockMultipleWeb3TransactionData } from "@data/mocks"
 
 jest.mock("react-truncate-string")
 
 describe("<BlockSingleTrx />", () => {
   it("renders correctly", () => {
-    const tree = renderWithTheme("dark", <BlockSingleTrx />)
+    const tree = renderWithTheme(
+      "dark",
+      <BlockSingleTrx transactions={mockMultipleWeb3TransactionData} />
+    )
     expect(tree).toMatchSnapshot()
   })
 })
