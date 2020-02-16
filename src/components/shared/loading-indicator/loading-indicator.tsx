79504b1f374fe3e998a4cd3c8db919e935b0deb3
Andy Hook <hello@andy-hook.co.uk>
2020-02-16 12:47:25 +0000

    Fake a simple load

--- src/components/shared/loading-indicator/loading-indicator.tsx
+++ src/components/shared/loading-indicator/loading-indicator.tsx
@@ -1,11 +1,45 @@
-import React, { memo } from "react"
+import React, { memo, useState, useEffect } from "react"
 import styled from "styled-components"
 import { layout } from "@style/design-tokens"
+import { Spring } from "react-spring/renderprops"
 import { useLoadingStatusContext } from "@providers/loading-status-provider/loading-status-provider"
 
 const LoadingIndicator: React.FunctionComponent = memo(() => {
   const { loading } = useLoadingStatusContext()
-  return <>{loading && <LoadingContainer />}</>
+
+  const initialPercent = 0
+
+  const [progress, setProgress] = useState<number>(initialPercent)
+
+  const loadingSpring = { mass: 1, tension: 50, friction: 100 }
+  const loadedSpring = { mass: 1, tension: 300, friction: 20 }
+
+  useEffect(() => {
+    if (loading) {
+      setProgress(40)
+    } else {
+      setProgress(100)
+    }
+  }, [loading])
+
+  return (
+    <>
+      <LoadingContainer>
+        <Spring
+          config={loading ? loadingSpring : loadedSpring}
+          from={{ percent: initialPercent }}
+          to={{ percent: progress }}
+        >
+          {({ percent }) => (
+            <LoadingBar
+              style={{ transform: `translate3d(-${100 - percent}%,0,0)` }}
+            />
+          )}
+        </Spring>
+      </LoadingContainer>
+      }
+    </>
+  )
 })
 
 const LoadingContainer = styled.div`
@@ -16,9 +50,20 @@ const LoadingContainer = styled.div`
   top: 0;
   left: 0;
   width: 100%;
-  background-color: red;
 
   height: 10px;
+`
+
+const LoadingBar = styled.div`
+  position: absolute;
+
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+
+  background-color: red;
 `
 
 export default LoadingIndicator
