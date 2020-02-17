7185d258dbd6bd7937f2eb8e4f968c8ce037144d
Andy Hook <hello@andy-hook.co.uk>
2020-02-17 20:37:46 +0000

    Remove redundant pages, update font-cache

--- src/components/page-content/about.tsx
+++
@@ -1,8 +0,0 @@
-import React, { memo } from "react"
-import Page from "@components/shared/page/page"
-
-const About: React.FunctionComponent = memo(() => {
-  return <Page>About page</Page>
-})
-
-export default About
